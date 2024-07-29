510fdee08b0d6a0c359b7c11ac3183de90130c15
Srikannan-S <159158997+Srikannan-S@users.noreply.github.com>
2024-07-29 17:09:17 +0530

    Add files via upload

---
+++ Convert_img_2_Grey_Scale.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Read the image
+img=cv2.imread(r"C:\Users\srika\Desktop\AIML\DAY_3\flower.png")
+
+# Convert the image into grey scale
+Gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# Save the gray image
+cv2.imwrite("grey.png",Gray_img)
+
+# Display both image
+# The names Original and Gray as optional it will display in Result
+cv2.imshow('Original',img)  
+cv2.imshow('Gray',Gray_img)
+
+
+cv2.waitKey(5000)
+cv2.destroyAllWindows()
